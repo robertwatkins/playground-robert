3729694ba72ae0be04cc2450bb17e8fe15a48d3a
robertwatkins <robert@watkins.net>
2015-04-22 20:10:52 -0500

    update

--- plugin/v0/popup.js
+++ plugin/v0/popup.js
@@ -88,15 +88,47 @@ function getImageUrl(searchTerm, callback, errorCallback) {
   x.send();
 }
 
+function getNote(url, callback, errorCallback) {
+	
+	callback('Sample Note');
+	
+	onerror = function() {
+		errorCallback('Unable to load note for this site');
+	};
+}
+
 function renderStatus(statusText) {
   document.getElementById('status').textContent = statusText;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+  console.log('start loading data');
   getCurrentTabUrl(function(url) {
     // Put the image URL in Google search.
     renderStatus('Notes for ' + url);
+    getNote (url, function(note) {
+		document.getElementById('notes').value=note;
+	}, function(errorMessage){
+		renderStatus(url + " + errorMessage);
+	});
+	
+    getImageUrl(url, function(imageUrl, width, height) {
 
-
+      renderStatus('Search term: ' + url + '\n' +
+          'Google image search result: ' + imageUrl);
+      var imageResult = document.getElementById('image-result');
+	  
+      // Explicitly set the width/height to minimize the number of reflows. For
+      // a single image, this does not matter, but if you're going to embed
+      // multiple external images in your page, then the absence of width/height
+      // attributes causes the popup to resize multiple times.
+      imageResult.width = width;
+      imageResult.height = height;
+      imageResult.src = imageUrl;
+      imageResult.hidden = false;
+      document.getElementById('notes').value=imageUrl;
+    }, function(errorMessage) {
+      renderStatus('Cannot display image. ' + errorMessage);
+    });
   });
 });
