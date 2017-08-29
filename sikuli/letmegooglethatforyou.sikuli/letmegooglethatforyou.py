3c935f81ac274699d5235770109cb76a38559a55
Robert Watkins <robert@watkins.net>
2017-08-29 17:57:29 -0500

    first version of 'let me google that for you'

---
+++ sikuli/letmegooglethatforyou.sikuli/letmegooglethatforyou.py
@@ -0,0 +1,20 @@
+click("1503877602839.png")
+wait(Pattern("1503877845868.png").similar(0.95))
+click(Pattern("1503877845868.png").similar(0.95))
+wait("1503877104119.png")
+click("1503877104119.png")
+type("www.google.com"+ Key.ENTER)
+wait("1503877160410.png")
+logo = find("1503877160410.png")
+logo.highlight(4)
+popup("Google is your friend")
+search = input("What do you want to know?","Type your question here")
+
+type("1503877308405.png",search)
+click("1503877367605.png")
+wait("1503877433623.png")
+popup( "Now you have a variety of options that may answer your question.")
+
+
+
+            
