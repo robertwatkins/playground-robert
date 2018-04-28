6dfc09fd7d91bb64dd8d52e5ed98b26a32ce9a01
Robert Watkins <robert@watkins.net>
2018-04-28 15:56:29 -0500

    changed method/contract names

--- blockchain/solidity/src/tictactoe/v1/test/tests.sol
+++ blockchain/solidity/src/tictactoe/v1/test/tests.sol
@@ -2,8 +2,8 @@ import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/tictactoe.sol";
 
-contract TestMetacoin {
-  function testInitialBalanceUsingDeployedContract() {
+contract TestTicTacToe {
+  function testShowWinnerUsingDeployedContract() {
     tictactoe game = tictactoe(DeployedAddresses.tictactoe());
 
     bytes1 expected = 0x00;
