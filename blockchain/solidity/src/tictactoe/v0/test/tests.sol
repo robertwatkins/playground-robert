e482f25b29aab98dd5c9f356a17230642e1dac99
Robert Watkins <robert@watkins.net>
2018-05-01 20:21:30 -0500

    reverted test run from solidity that doesn't catch 'revert' well

--- blockchain/solidity/src/tictactoe/v0/test/tests.sol
+++ blockchain/solidity/src/tictactoe/v0/test/tests.sol
@@ -5,13 +5,23 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/tictactoe.sol";
 
 contract tests {
-  function testShowWinnerUsingDeployedContract() public {
+  function testValidGameNoWinner() public {
     tictactoe game = tictactoe(DeployedAddresses.tictactoe());
 
+    bytes9 gameState = 0x111100aaaa00000000;
+    bytes1 expected = 0x00;
+
+    Assert.equal(game.showWinner(gameState), expected, "No winner expected for this game. ");
+  }
+
+  function testValidGameWinnerFound() public {
+    tictactoe game = new tictactoe();
     bytes9 gameState = 0x111100aaaaaa110000;
     bytes1 expected = 0xaa;
 
-    Assert.equal(game.showWinner(gameState), expected, "No winner expected for new game.");
+    Assert.equal(game.showWinner(gameState), expected, "Winner expected for this game.");
   }
-}
 
+  /* Removed test that resulted in a 'revert' since solidity does not handle this well at the moment */
+
+}
