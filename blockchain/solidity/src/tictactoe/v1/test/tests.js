8e6b286a09a18667e857d363ed0ccce322d7d3e4
Robert Watkins <robert@watkins.net>
2018-05-06 17:59:53 -0500

    updated tests

--- blockchain/solidity/src/tictactoe/v1/test/tests.js
+++ blockchain/solidity/src/tictactoe/v1/test/tests.js
@@ -6,15 +6,20 @@ const tictactoe = artifacts.require("tictactoe");
 
 contract('Tictactoe Test', async (accounts) => {
 
-  it("should not have a winner for this game", async () => {
+  it("should have a winner for this game", async () => {
     let game = await tictactoe.deployed();
-    let winner = await game.showWinner('0x111100aaaa00000000');
-    assert.equal(winner.valueOf(), '0x00');
+    await game.makeMove('0x11',0);
+    await game.makeMove('0xaa',3);
+    await game.makeMove('0x11',1);
+    await game.makeMove('0xaa',4);
+    let winner = await game.makeMove('0x11',2);
+    console.log('winner:' + winner.valueOf());
+    assert.equal(winner.valueOf(), '0x11');
   });
 
-  it("should not process invalid games", async () => {
+  it("should not process invalid moves", async () => {
     let game = await tictactoe.deployed();
-    await assertRevert(game.showWinner('0x111100aaaaaa000000'));
+    await assertRevert(game.makeMove('0x11',11));
   });
 
 })
--- blockchain/solidity/src/tictactoe/v1/test/tests.sol
+++ blockchain/solidity/src/tictactoe/v1/test/tests.sol
@@ -8,20 +8,10 @@ contract tests {
   function testValidGameNoWinner() public {
     tictactoe game = tictactoe(DeployedAddresses.tictactoe());
 
-    bytes9 gameState = 0x111100aaaa00000000;
+    bytes1 player = 0x11;
+    uint256 moveLocation = 0;
     bytes1 expected = 0x00;
 
-    Assert.equal(game.showWinner(gameState), expected, "No winner expected for this game. ");
+    Assert.equal(game.makeMove(player,moveLocation), expected, "No winner expected for this game. ");
   }
-
-  function testValidGameWinnerFound() public {
-    tictactoe game = new tictactoe();
-    bytes9 gameState = 0x111100aaaaaa110000;
-    bytes1 expected = 0xaa;
-
-    Assert.equal(game.showWinner(gameState), expected, "Winner expected for this game.");
-  }
-
-  /* Removed test that resulted in a 'revert' since solidity does not handle this well at the moment */
-
 }
