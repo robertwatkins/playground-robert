7fd93871d3cb56e160d9451e37906245b3eea76e
Robert Watkins <robert@watkins.net>
2018-05-06 20:03:56 -0500

    added (un-implemented) test cases

--- blockchain/solidity/src/tictactoe/v1/test/tests.js
+++ blockchain/solidity/src/tictactoe/v1/test/tests.js
@@ -17,9 +17,33 @@ contract('Tictactoe Test', async (accounts) => {
     assert.equal(winner.valueOf(), '0x11');
   });
 
-  it("should not process invalid moves", async () => {
+  it("should not process invalid moves.", async () => {
     let game = await tictactoe.deployed();
     await assertRevert(game.makeMove('0x11',11));
+  });
+
+  it("should allow a game to continue after an invalid move.", async() => {
+    assert.fail("Not Implemented");
+  });
+
+  it("should start with an empty game state.", async() => {
+    assert.fail("Not Implemented");
+  });
+
+  it("should show the game state that includes the most recent move.", async() => {
+    assert.fail("Not Implemented");
+  });
+
+  it("After a winning game, the game state should be empty.", async() => {
+    assert.fail("Not Implemented");
+  });
+
+  it("Only the X player can go first.", async() => {
+    assert.fail("Not Implemented");
+  });
+
+  it("if no winner exists and no moves are left, then it's a cat's game.", async() => {
+    assert.fail("Not Implemented");
   });
 
 })
